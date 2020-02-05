3c8c86d6eaef6e3c6a1af0002db373256c83737d
Uniboy-ROS <59160025+Uniboy-ROS@users.noreply.github.com>
2020-02-05 16:58:23 +0800

    can module connect success

--- client_server_example/client.py
+++ client_server_example/client.py
@@ -4,19 +4,23 @@ import socket
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # socket.AF_INET = IPv4 , socket.SOCK_STREAM = socket(TCP) , DGRAM = socket(UDP)
 
-client.connect(('192.168.4.100', 8888))
+client.connect(('192.168.4.102', 8886))
 # connection address(host,port)
 
-data = input()
-#輸入 要傳輸的資料
+while True:
 
-data = data.encode('utf-8')
-#將字串編碼為byte
+    data = input('enter message: ')
+    #輸入 要傳輸的資料
+    hex_data = bytes.fromhex(data)
 
-client.sendall(data)
-#傳輸資料
+    #data = data.encode('utf-8')
+    #將字串編碼為byte
 
-rec_data = client.recv(1024)
-print(b'form server receive:' + rec_data)
+    client.sendall(hex_data)
+    #傳輸資料
+
+    #rec_data = client.recv(1024)
+    #hex_string = rec_data.hex()
+    #print('form server receive:' + hex_string)
 
 client.close()
--- client_server_example/server.py
+++ client_server_example/server.py
@@ -4,7 +4,7 @@ import socket
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # socket.AF_INET = IPv4 , socket.SOCK_STREAM = socket(TCP) , DGRAM = socket(UDP)
 
-server.bind(('192.168.4.100', 8888))
+server.bind(('192.168.4.100', 8887))
 # connection address(host,port)
 
 server.listen(5)
@@ -26,12 +26,17 @@ sock_name = connect.getsockname()
 print(u'the client %s:%s has connected.' % (host, port))
 print('The peer name is %s and sock name is %s' % (peer_name, sock_name))
 
+while True:
+    data = connect.recv(1024)
+    # hex_bytes = bytes.fromhex(data)
+    hex_string = data.hex()
+    print(type(hex_string))
+    #接收client的data
 
-data = connect.recv(1024)
-#接收client的data
-
-connect.sendall(b'your words has received.')
-print(b'the client say:' + data)
+    connect.sendall(b'your words has received.')
+    print('the client say:' + hex_string)
+    # print(hex_string)
+    # print(data)
 
 
 server.close()
